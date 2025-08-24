04a4a9663804c5b838b0df769974de42347b8075
mrifqyabdh <mrifqyabdh@gmail.com>
2025-08-24 23:15:22 +0900

    Completed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@ const input = document.querySelector('#input');
 const priority = document.querySelector('#priority');
 const date = document.querySelector('#date');
 const btnDelete = document.querySelector('#delete');
+const dateToday = document.querySelector('#date-today');
 
+
+let today = new Date();
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const Today = today.toLocaleDateString('en-US', options);
+
+dateToday.textContent = Today;
 let DataTodo = [];
 
 // === PROFILE HANDLING (event delegation) ===
@@ -11,37 +18,65 @@ document.body.addEventListener('click', function(event) {
     if (event.target.id === 'btnSubmitProfile') {
         const username = document.querySelector('#username').value;
         const position = document.querySelector('#position').value;
+        
         const div = document.createElement('div');
         div.innerHTML = `
-            <p><b>${username}</b></p>
-            <p>${position}</p>
-            <button id="edit">Edit</button>
+            <button class="btn-profile" id="edit">EDIT</button>
         `;
         const container = document.querySelector('#profile');
+        const container1 = document.querySelector('#profile-h1');
+        container1.textContent = `${username} - ${position}`;
         container.innerHTML = '';
         container.appendChild(div);
+        container.parentElement.classList.replace('profile-card', 'profile-card-done');
+
 
     } else if (event.target.id === 'edit') {
         const container = document.querySelector('#profile');
         container.innerHTML = `
+            <p>Nama :</p>
             <input type="text" id="username" placeholder="Masukkan Nama" required>
+            <p>Jabatan :</p>
             <input type="text" id="position" placeholder="Masukkan Jabatan" required> 
-            <button id="btnSubmitProfile">submit</button>
+            <button class="btn-profile" id="btnSubmitProfile">SUBMIT</button>
         `;
+        container.parentElement.classList.replace('profile-card-done', 'profile-card');
+        const container1 = document.querySelector('#profile-h1');
+        container1.textContent = `Profile`;
     }
 })
 const RenderData = () => {
     const container = document.querySelector('#container');
+    const containerDone = document.querySelector('#container-done');
     container.innerHTML = '';
+    containerDone.innerHTML = '';
     DataTodo.forEach((todo) => {
+        let date = new Date(todo.Date);
+        let formatted = date.toLocaleDateString('en-US', {
+            day: 'numeric',
+            month: 'long',   // 👉 full month name
+            year: 'numeric'
+        });
         const div = document.createElement('div');
         div.className = 'todo-item';
         div.innerHTML = `
-            <p class="${todo.done ? 'done' : ''}">${todo.ToDo} - ${todo.Priority} - ${todo.Date}</p>
             <input type="checkbox" class="todo-check" data-id="${todo.id}" ${todo.done ? "checked" : ""}>
+            
+            <p class="${todo.done ? 'done' : ''}">${todo.ToDo} - ${todo.Priority} Priority - ${formatted}</p>
+            
             <button class="deleteItem" data-id="${todo.id}">Delete</button>
         `;
         container.appendChild(div);
+        if (div.firstElementChild.nextElementSibling.classList.contains('done')) {
+            const divDone = document.createElement('div');
+            divDone.className = 'todo-item';
+            divDone.innerHTML = `
+            <input type="checkbox" class="todo-check" data-id="${todo.id}" ${todo.done ? "checked" : ""}>
+                <p class="${todo.done ? 'done' : ''}">${todo.ToDo} - ${todo.Priority} Priority - ${formatted}</p>
+                <button class="deleteItem" data-id="${todo.id}">Delete</button>
+            `;
+            containerDone.appendChild(divDone);
+        }
     });
 };
 
@@ -75,9 +110,9 @@ document.body.addEventListener('change', (event) => {
         DataTodo = DataTodo.map(todo =>
             todo.id === id ? { ...todo, done: checked } : todo
         );
-        const p = event.target.previousElementSibling;
+        const p = event.target.nextElementSibling;
         p.classList.toggle("done", event.target.checked); 
-        console.log(DataTodo);
+        RenderData();
     }
 });
 
